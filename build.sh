9a047ceed8da6fe882cab02288ee9996b70402b0
chunseok lee <chunseoklee@gmail.com>
2017-01-23 11:14:23 +0900

    add link dir

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-export CORECLR_SRC=$HOME/coreclr/src
+#!/bin/bash
+export CORECLR_SRC=$HOME/dev/coreclr/src
 mkdir bin > /dev/null 2>&1
 
 OSName=$(uname -s)
@@ -31,7 +32,8 @@ esac
 
 if [ $__BuildOS = "Linux" ]; then
 
-    clang++-3.5 -DPAL_IMPLEMENTATION -DFEATURE_PAL -D__LINUX__ -DPLATFORM_UNIX -DPAL_STDCPP_COMPAT -I./inc -I$CORECLR_SRC/inc -I$CORECLR_SRC/pal/inc -I$CORECLR_SRC/pal/inc/rt -pthread -std=c++11 -g dbg.cpp cordebug_i.cpp -o bin/dbg -L$CORECLR_SRC/../bin/Product/Linux.x64.Debug/lib -ldl -lunwind-x86_64 -lunwind-generic -lunwind-ptrace -lunwind -luuid -lpalrt -lcoreclrpal
+    clang++-3.5 -DPAL_IMPLEMENTATION -DFEATURE_PAL -D__LINUX__ -DPLATFORM_UNIX -DPAL_STDCPP_COMPAT -I./inc -I$CORECLR_SRC/inc -I$CORECLR_SRC/pal/inc -I$CORECLR_SRC/pal/inc/rt -pthread -std=c++11 -g dbg.cpp cordebug_i.cpp -o bin/dbg -L$CORECLR_SRC/../bin/Product/Linux.x64.Debug/lib -ldl -lunwind-x86_64 -lunwind-generic -lunwind-ptrace -lunwind -luuid \
+-L/home/twoflower/dev/coreclr/bin/Product/Linux.x64.Debug/lib  -lpalrt -lcoreclrpal
 
 elif [ $__BuildOS = "OSX" ]; then
 
